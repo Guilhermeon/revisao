abe14765b23c9841fa1e9fa1723d08a45f2680de
Guilhermeon <guilhermeaugukiko@gmail.com>
2019-08-01 19:32:31 -0300

    Minha primeira versão: adição do arquivo exemplo1.php

---
+++ exemplo1.php
@@ -0,0 +1,41 @@
+<?php
+    
+    // Variaveis Primitivas
+    
+        // variavel inteira
+        $var_inteira = 45;
+        // variavel float
+        $var_ponto_flutuante = 3.14;
+        //string de caracteres
+        $var_string = "Uma String";
+        // variavel booleana
+        $var_boolean = true;
+        // variavel nula
+        $var_null = null;
+
+    // ******************************************
+
+    // Variaveis compostas
+
+        //vetores e matrizes
+        // tipos de dados compostos heterogeneos associativos
+        $v = array("a","b",10, true, "xyz", 3.89);
+
+        print_r($v);
+
+        $m[0][0]="valor da posição 0x0";
+        $m[0][1]="valor da posição 0x1";
+        $m[1][0]="valor da posição 0x1";
+        $m[1][1]="valor da posição 0x1";
+
+    //**************************************************************
+
+    
+
+
+
+    
+
+
+
+?>
